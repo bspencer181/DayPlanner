c34e9d6cd1616575d252bebed26d4b1640777d7f
Beth Spencer <beth.spencer@student.labette.edu>
2020-10-14 18:10:26 -0500

    changed HTML

---
+++ script.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    //STEP 1 color code based on time USE moment .js
+    var hour = moment().hours();
+    console.log(hour)
+    //i found the time
+    //write a function to check if the moment is passed my time div
+    //and grey out hours that are passed
+    //STEP 2 when i click on middle col i can enter text
+    //make text input
+    //Step 3 click on Save button text from step 2 is saved in local storage and persists after i refresh
+    //write a function to grab that text value and save it to local storage
+  });
