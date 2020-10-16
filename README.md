52cdc98c1a3f1326b62219dd42d2bcd2f5bdd1e1
Beth Spencer <beth.spencer@student.labette.edu>
2020-10-15 22:23:59 -0500

    added readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # DayPlanner
+Created a calendar application that lets users enter daily tasks. This application changes colors based on the current time.
+
+<img src="https://github.com/bspencer181/DayPlanner/blob/main/Capture.JPG?raw=true">
+
+## Built With:
+ -HTML
+ -CSS
+ -JavaScript
+ -jQuery
+
+ ### Links
+ https://github.com/bspencer181/DayPlanner
+ https://bspencer181.github.io/DayPlanner/
--- script.js
+++ script.js
@@ -4,20 +4,28 @@ $(document).ready(function() {
     (console.log(hour))
 
     var date = moment().format("dddd, MMMM Do YYYY");
-// var currentTime = moment().format('H');
-// console.log(currentTime);
 
-//-------append date----------//
 $("#currentDay").append(date);
 
-    // write function that checks current time
-    //and grey out hours that are passed
-    //Step 3 click on Save button text from step 2 is saved in local storage and persists after i refresh
-    //write a function to grab that text value and save it to local storage
+$("currentDay").each(function () {
+    var hour = parseInt($(this).attr("hour"));
+    if (hour < now) {
+        $(this).addClass("future");
+    }
 
-      
 
-    
+    if (hour > now) {
+        $(this).addClass("past")
+    }
+
+    if (hour === now) {
+        $(this).addClass("present")
+    }
+
+
+
+})
+ 
     
   });
 
